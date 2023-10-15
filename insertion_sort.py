19c8343654c108297113f2ad9385015db23fdbf5
Mona Dastar <-c>
2023-10-14 22:46:49 -0400

    change the body of the code

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,18 +1,11 @@
 def insertion_sort(input_list):
-    sorted_list = input_list.copy()  
 
-    for i in range(1, len(sorted_list)):
-        current = sorted_list[i]
-        j = i - 1
+    for i in range(1, len(input_list)):
+        j=i
+        while j>0 and input_list[j-1]>input_list[j]:
+            input_list[j-1],input_list[j]=input_list[j],input_list[j-1]
+            j-=1
+    return(input_list)
 
-        while j >= 0 and sorted_list[j] > current:
-            sorted_list[j + 1] = sorted_list[j]
-            j -= 1
-
-        sorted_list[j + 1] = current
-
-    return sorted_list
-
-list1 = [5, 1, 9, 3, 7, 2]
-sorted_list = insertion_sort(list1)
-print(sorted_list)
+input_list=[5,2,4,20,1,6]
+print(insertion_sort(input_list))
